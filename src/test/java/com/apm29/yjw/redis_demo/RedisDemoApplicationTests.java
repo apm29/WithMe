bccf1ad223fb442cc44ab930942b7aa1299287e7
yingjiawei <yjw999wow@163.com>
2018-09-05 17:23:10 +0800

    add

--- src/test/java/com/apm29/yjw/redis_demo/RedisDemoApplicationTests.java
+++ src/test/java/com/apm29/yjw/redis_demo/RedisDemoApplicationTests.java
@@ -2,15 +2,25 @@ package com.apm29.yjw.redis_demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class RedisDemoApplicationTests {
 
     @Test
     public void contextLoads() {
     }
 
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void homeResponse() {
+        String body = this.restTemplate.getForObject("/", String.class);
+        assertThat(body).contains("{");
+    }
 }
